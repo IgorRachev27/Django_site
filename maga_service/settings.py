940ee236c65b4230fbc25065f4a6bec96aadb0c8
Игорь Рачев <igorrachev@MacBook-Pro-Igor.local>
2022-02-15 16:07:06 +0300

    Added files and changes required for deployment to heroku

--- maga_service/settings.py
+++ maga_service/settings.py
@@ -129,9 +129,9 @@ STATIC_URL = os.path.join(BASE_DIR, '/static/')
 STATICFILES_DIRS = [
     BASE_DIR / "maga_service/static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = os.path.join(BASE_DIR, '/media/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
