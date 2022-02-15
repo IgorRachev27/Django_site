072977b772f3342907d5912632d25772a626d8b6
Игорь Рачев <igorrachev@MacBook-Pro-Igor.local>
2022-02-15 12:06:15 +0300

    Added files and changes required for deployment to heroku

--- maga_service/settings.py
+++ maga_service/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = os.path.join(BASE_DIR, '/static/')
 STATICFILES_DIRS = [
     BASE_DIR / "maga_service/static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = os.path.join(BASE_DIR, '/media/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
