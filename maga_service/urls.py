292971b9620cf1d90cb6d00e55d740da7544c81a
Игорь Рачев <igorrachev@MacBook-Pro-Igor.local>
2022-02-27 21:53:51 +0300

    Added files and changes required for deployment to heroku

--- maga_service/urls.py
+++ maga_service/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from crm.views import main_page, thanks, gallery
+from crm.views import main_page, thanks, gallery, order
 from django.conf.urls.static import static
 from django.conf import settings
 from .settings import DEBUG
